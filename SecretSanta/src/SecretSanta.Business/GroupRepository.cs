543feb3e36f4419211aa10d2ecf66fed5ea3694a
Nicholas Johnson <nicholasj@eagles.ewu.edu>
2021-05-24 13:34:19 -0700

    Updated mock data to Bojak Horseman theme instead of fixing the Generate Assignments button that is currently doing nothing which actually matters towards my grade

--- SecretSanta/src/SecretSanta.Business/GroupRepository.cs
+++ SecretSanta/src/SecretSanta.Business/GroupRepository.cs
@@ -37,14 +37,42 @@ namespace SecretSanta.Business
             return MockData.Groups.Remove(id);
         }
 
+        // https://stackoverflow.com/questions/273313/randomize-a-listt
+        private void RandomizeListOrder<user>(IList<user> list)
+        {
+            Random rng = new Random();
+            int n = list.Count;
+            while (n > 1)
+            {
+                n--;
+                int k = rng.Next(n + 1);
+                user value = list[k];
+                list[k] = list[n];
+                list[n] = value;
+            }
+        }
+
         public AssignmentResult GenerateAssignments(int id)
         {
             if (MockData.Groups[id].Users.Count() <= 2)
-                return AssignmentResult.Error("There are not enough users in this group");
+            {
+                return AssignmentResult.Error("Group minimum is 3 users");
+            }
+
+
+            List<User> users = MockData.Groups[id].Users;
+            RandomizeListOrder(users);
 
             for (int i = 0; i < MockData.Groups[id].Users.Count(); i++)
             {
-                MockData.Groups[id].Assignments[i] = new(MockData.Groups[id].Users[i], MockData.Groups[id].Users[i]);
+                if (i < users.Count - 1)
+                {
+                    MockData.Groups[id].Assignments.Add(new Assignment(users[i], users[i + 1]));
+                }
+                else
+                {
+                    MockData.Groups[id].Assignments.Add(new Assignment(users[i], users[0]));
+                }
             }
             return AssignmentResult.Success();
         }
--- SecretSanta/src/SecretSanta.Web/Data/MockData.cs
+++ SecretSanta/src/SecretSanta.Web/Data/MockData.cs
@@ -7,25 +7,27 @@ namespace SecretSanta.Web.Data
     {
         public static List<UserViewModel> Users = new List<UserViewModel>
         {
-            new UserViewModel { Id = 1, FirstName = "Inigo", LastName = "Montoya" },
-            new UserViewModel { Id = 2, FirstName = "Princess", LastName = "Buttercup" },
-            new UserViewModel { Id = 3, FirstName = "Prince", LastName = "Humperdink" },
-            new UserViewModel { Id = 4, FirstName = "Count", LastName = "Rugen" },
-            new UserViewModel { Id = 5, FirstName = "Miracle", LastName = "Max" },
+            new UserViewModel { Id = 1, FirstName = "BoJack", LastName = "Horseman" },
+            new UserViewModel { Id = 2, FirstName = "Princess", LastName = "Carolyn" },
+            new UserViewModel { Id = 3, FirstName = "Diane", LastName = "Nguyen" },
+            new UserViewModel { Id = 4, FirstName = "Mr.", LastName = "Peanutbutter" },
+            new UserViewModel { Id = 5, FirstName = "Todd", LastName = "Chavez" },
+            new UserViewModel { Id = 6, FirstName = "Charley", LastName = "Witherspoon" },
+            new UserViewModel { Id = 7, FirstName = "Judah", LastName = "Mannowdog" },
         };
 
         public static List<GroupViewModel> Groups = new List<GroupViewModel>
         {
-            new GroupViewModel { Id = 1, Name = "GoonSquad" },
-            new GroupViewModel { Id = 2, Name = "Rodents Of Unusual Size" },
+            new GroupViewModel { Id = 1, Name = "Princess and Judah Wedding" },
+            new GroupViewModel { Id = 2, Name = "The Lonely One Person Exchange" },
         };
 
         public static List<GiftViewModel> Gifts = new List<GiftViewModel>
         {
-            new GiftViewModel { Id = 1, Title = "Drone", Description = "A fun little way to get a birds eye view", Url="https://www.google.com", Priority = 2, UserId = 1 },
-            new GiftViewModel { Id = 2, Title = "Rat Poison", Description = "Hopefully a way to get rid of those unusual size rodents", Url="https://www.google.com", Priority = 1, UserId = 1 },
-            new GiftViewModel { Id = 3, Title = "Drone", Description = "A fun little way to get a birds eye view", Url="https://www.google.com", Priority = 2, UserId = 2 },
-            new GiftViewModel { Id = 4, Title = "Rat Poison", Description = "Hopefully a way to get rid of those unusual size rodents", Url="https://www.google.com", Priority = 1, UserId = 2 },
+            new GiftViewModel { Id = 1, Title = "D", Description = "From the Hollywoo sign", Url="https://www.google.com", Priority = 2, UserId = 1 },
+            new GiftViewModel { Id = 2, Title = "Horsin' Around Disk Set", Description = "All 9 Episodes of the classic family sitcom", Url="https://www.google.com", Priority = 1, UserId = 1 },
+            new GiftViewModel { Id = 3, Title = "Ivy Tran, Food Court Detective", Description = "A teal middle-grade fiction book that has the title in purple lettering. It has a picture of a red fast food tray with a hamburger and soda on it.", Url="https://www.google.com", Priority = 2, UserId = 2 },
+            new GiftViewModel { Id = 4, Title = "Binoculars", Description = "Good enough to enjoy the view from all the way up", Url="https://www.google.com", Priority = 1, UserId = 2 },
         };
     }
 }
